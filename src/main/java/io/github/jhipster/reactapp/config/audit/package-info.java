1dcaa301df808440fa7e25586c4db8b04a10db1b
damian.winnicki <damian.winnicki>
2018-09-12 10:00:23 +0200

    first commit

---
+++ src/main/java/io/github/jhipster/reactapp/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package io.github.jhipster.reactapp.config.audit;
---
+++ src/main/java/io/github/jhipster/reactapp/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package io.github.jhipster.reactapp.repository;
+
+import io.github.jhipster.reactapp.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
